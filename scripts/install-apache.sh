e729827a920433c7353f4ffe93a8f55fd7223123
Sthitaprajna Das <das@aiceberg.ai>
2023-09-17 17:50:53 +0530

    moved correct folder

---
+++ scripts/install-apache.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+if [[ -z $(apache2 -v 2>/dev/null) ]] && [[ -z $(httpd -v 2>/dev/null) ]]; then 
+    echo "Apache not found"; 
+    sudo apt update -y ; 
+    sudo apt install apache2 -y ;
+    sudo systemctl start apache2 ;
+    sudo systemctl enable apache2;
+    sudo ufw allow 'Apache';
+    echo "Apache installed sucessfully"; 
+elif
+    echo "Apache was already available";    
+fi

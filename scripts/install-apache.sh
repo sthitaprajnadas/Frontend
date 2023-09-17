3e291b2c75f8725299aa7e4ce87e437efb9855f8
Sthitaprajna Das <das@aiceberg.ai>
2023-09-17 21:47:06 +0530

    copying script first2

--- scripts/install-apache.sh
+++ scripts/install-apache.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-if [[ -z $(apache2 -v 2>/dev/null) ]] && [[ -z $(httpd -v 2>/dev/null) ]]; then 
+if [[ -z $(apache2 -v 2>/dev/null) ]] && [[ -z $(httpd -v 2>/dev/null) ]]; 
+then 
     echo "Apache not found"; 
     sudo apt update -y ; 
     sudo apt install apache2 -y ;

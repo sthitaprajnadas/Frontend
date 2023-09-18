81e937aa5d326039442b16cfc3790d46d3f6d229
Sthitaprajna Das <das@aiceberg.ai>
2023-09-18 18:32:35 +0530

    on dev

--- extras/scripts/install-apache.sh
+++ extras/scripts/install-apache.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 if [[ -z $(apache2 -v 2>/dev/null) ]] && [[ -z $(httpd -v 2>/dev/null) ]]
 then 
     echo "Apache not found"; 
@@ -7,7 +7,13 @@ then
     sudo systemctl start apache2 ;
     sudo systemctl enable apache2;
     sudo ufw allow 'Apache';
+    sudo usermod -aG root ubuntu
+    sudo chmod -R 777 /var/www
+    sudo chmod -R 777 /etc/apache2/sites-enabled
+    mv /tmp/extras/apache/aiceberg.wsgi /var/www/
+    mv /tmp/extras/apache/000-default.conf /etc/apache2/sites-enabled/
     echo "Apache installed sucessfully"; 
 else
+    sudo chmod -R 777 /var/www
     echo "Apache was already available";    
 fi
